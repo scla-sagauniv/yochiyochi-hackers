3ce7211d43f8781d4be1908cc966d49c3d0049bd
Ririri6 <liaiwantian@gmail.com>
2024-05-18 16:10:15 +0900

    フロントを作成した

---
+++ app/ria/page.tsx
@@ -0,0 +1,100 @@
+'use client'
+import * as React from 'react';
+import './main.css';
+import { useState } from 'react'
+import {Box,Button,Input,FormControl,FormLabel,Heading,useToast, background,} from '@chakra-ui/react'
+import {List,ListItem,ListIcon,OrderedList,UnorderedList,} from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+
+export default function Ria() {
+  const { register, handleSubmit } = useForm()
+
+  type TaskType = {
+    id: number
+    task: string
+    isDone: boolean
+  }
+  const [text, getTask] = useState<string>('')
+  const [tasks, setTask] = useState<TaskType[]>([])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    getTask(e.target.value)
+  }
+
+  const handleClick = (): void => {
+    setTask([...tasks, { id: tasks.length++, task: text, isDone: false }])
+  }
+  function onSubmit(values) {
+    console.log('ssssss', values.task)
+    handleClick();
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl
+          id="taskname"
+          onSubmit={handleSubmit(data => {
+            console.log('data: ', data)
+          })}
+        >
+          <FormLabel htmlFor="task">タスクの追加</FormLabel>
+          <Input
+            type="text"
+            placeholder="タスクを入力"
+            id="task"
+            {...register('task')}
+          />
+          <Button colorScheme="orange" size="md" type="submit" >追加</Button>
+        </FormControl>
+      </form>
+      
+      <div className="main" >
+        
+
+      <Tabs>
+        <TabList className="Tab">
+            <Tab>Do</Tab>
+            <Tab>Done</Tab>
+        </TabList>
+        <TabPanels>
+            <TabPanel>タスク
+                <div className='taskAll'>
+                    <ul>
+                         <li>課題
+                            <span>
+                                <Button className="DoneButton"colorScheme="teal" size="md">完了</Button>
+                            </span>
+                         </li> 
+                         
+                   </ul>
+        
+                </div>
+            </TabPanel>
+            <TabPanel>完了済み
+                <div className="DoneTask">
+                    <ul>
+                        <li>
+                            レポート
+                            <span>
+                                <Button className="ReDoButton" colorScheme="teal" size="md">取り消し</Button>
+                            </span>
+                        </li>
+                    </ul>
+
+                </div>
+            </TabPanel>
+        </TabPanels>
+      </Tabs>
+        
+
+      </div>
+
+      <div className="reset">
+          <Button colorScheme="orange" size="md">リセット</Button>
+      </div>
+
+      
+    </div>
+  )
+}
